bfc57d3269c8eeb8e366dd220cec29fa46e6ea23
Milos Bogdanovic <milosbogdanovic@Stefans-MacBook-Pro.local>
2019-05-10 22:26:03 +0200

    Pagination crawl.

--- src/amazoncrawl/amazoncrawl/settings.py
+++ src/amazoncrawl/amazoncrawl/settings.py
@@ -17,10 +17,12 @@ NEWSPIDER_MODULE = 'amazoncrawl.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'amazoncrawl (+http://www.yourdomain.com)'
-USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+# USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
+
+PROXY_POOL_ENABLED = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -40,32 +42,45 @@ ROBOTSTXT_OBEY = True
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
 #}
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'amazoncrawl.middlewares.AmazoncrawlSpiderMiddleware': 543,
 #}
 
+# DOWNLOADER_MIDDLEWARES = {
+#     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+#     'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+# }
+
+DOWNLOADER_MIDDLEWARES = {
+    # ...
+    'scrapy_proxy_pool.middlewares.ProxyPoolMiddleware': 610,
+    'scrapy_proxy_pool.middlewares.BanDetectionMiddleware': 620,
+    # ...
+}
+
+
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    'amazoncrawl.middlewares.AmazoncrawlDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
 #}
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
+# ITEM_PIPELINES = {
 #    'amazoncrawl.pipelines.AmazoncrawlPipeline': 300,
 #}
 
--- src/amazoncrawl/amazoncrawl/spiders/amazon_spider.py
+++ src/amazoncrawl/amazoncrawl/spiders/amazon_spider.py
@@ -5,6 +5,7 @@ from ..items import AmazoncrawlItem
 
 class AmazonSpiderSpider(scrapy.Spider):
     name = 'amazon'
+    page_num = 2
     allowed_domains = ['amazon.com']
     start_urls = ['https://www.amazon.com/s?k=boxing+gloves&ref=nb_sb_noss']
 
@@ -20,3 +21,8 @@ class AmazonSpiderSpider(scrapy.Spider):
         items['imagelink'] = imagelink
 
         yield items
+
+        next_page = 'https://www.amazon.com/s?k=boxing+gloves&page=' + str(AmazonSpiderSpider.page_num) + '&qid=1557483146&ref=sr_pg_2'
+        if AmazonSpiderSpider.page_num <= 5:
+            AmazonSpiderSpider.page_num += 1
+            yield response.follow(next_page, callback=self.parse)
